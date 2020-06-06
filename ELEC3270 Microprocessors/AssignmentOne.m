a50857bd745af125867ae80851246be1ccd47a12
MoonToMars <66174449+MoonToMars@users.noreply.github.com>
2020-06-06 09:40:19 -0400

    Week 2 of school

---
+++ ELEC3270 Microprocessors/AssignmentOne.m
@@ -0,0 +1,13 @@
+%% Assignement 1 Microprocessors
+% Author: Emmanuel Mati
+
+%inputs
+Q1 = [21013 13103 13720 27814 16300 12253];
+A1 = de2bi(Q1, 16)
+A1(4:6,:) = not(A1(4:6,:) - 0)
+
+%%
+ a = 75.707031250 * 2^8
+ b = 54.609375000 * 2^8
+ c = 57.324218750* 2^8
+ 
---
+++ ELEC4490 Sensors and Vision Systems/Assignment 2/Assignment2.m
@@ -0,0 +1,7 @@
+%% Image Processing Assignment
+% ELEC-4490 S2020
+% Lab assignment 1
+%
+clear all; %Deletes all variables
+close all; % Closes all figure windows
+clc; %Clears the command window
